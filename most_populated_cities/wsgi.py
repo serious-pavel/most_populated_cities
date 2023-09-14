e502d0d0fb1666d0cc8d05292204ae21067e53a9
pavel.makhnev <p_makhnev@ural-press.ru>
2023-09-14 19:38:52 +0200

    init django project

---
+++ most_populated_cities/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for most_populated_cities project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'most_populated_cities.settings')
+
+application = get_wsgi_application()
