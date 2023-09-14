524bfa53f2ea8a1b2ed9078971be91d43f680321
pavel.makhnev <p_makhnev@ural-press.ru>
2023-09-14 19:59:25 +0200

    create main app

---
+++ most_populated_cities_main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MostPopulatedCitiesMainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'most_populated_cities_main'
