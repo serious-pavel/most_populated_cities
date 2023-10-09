f9bc3f8bcac93be520eddeab1f2b08aecf129bd3
pavel.makhnev <p_makhnev@ural-press.ru>
2023-10-09 16:54:05 +0200

    register models for cities and countries

--- most_populated_cities_main/admin.py
+++ most_populated_cities_main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Country, City
 
 # Register your models here.
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    pass
+
