a9abb234df22bad47e022b1a1fb444f4167da67d
pavel.makhnev <p_makhnev@ural-press.ru>
2023-10-24 15:12:53 +0200

    add un region with property to Country

--- most_populated_cities_main/models.py
+++ most_populated_cities_main/models.py
@@ -15,8 +15,30 @@ class Country(models.Model):
         OCEANIA = "OC", _("Oceania")
         ANTARCTICA = "AN", _("Antarctica")
 
-        # def __str__(self):
-        #     return f"({self.label})"
+    class Region(models.TextChoices):
+        SOUTHERN_ASIA = "S_AS", _("Southern Asia")
+        NORTHERN_EUROPE = "N_EU", _("Northern Europe")
+        SOUTHERN_EUROPE = "S_EU", _("Southern Europe")
+        NORTHERN_AFRICA = "N_AF", _("Northern Africa")
+        POLYNESIA = "POLY", _("Polynesia")
+        MIDDLE_AFRICA = "M_AF", _("Middle Africa")
+        CARIBBEAN = "CARI", _("Caribbean")
+        ANTARCTICA = "ANTA", _("Antarctica")
+        SOUTH_AMERICA = "S_AM", _("South America")
+        WESTERN_ASIA = "W_AS", _("Western Asia")
+        AUSTRALIA_AND_NEW_ZEALAND = "A_NZ", _("Australia and New Zealand")
+        WESTERN_EUROPE = "W_EU", _("Western Europe")
+        EASTERN_EUROPE = "E_EU", _("Eastern Europe")
+        CENTRAL_AMERICA = "C_AM", _("Central America")
+        WESTERN_AFRICA = "W_AF", _("Western Africa")
+        NORTHERN_AMERICA = "N_AM", _("Northern America")
+        SOUTHERN_AFRICA = "S_AF", _("Southern Africa")
+        EASTERN_AFRICA = "E_AF", _("Eastern Africa")
+        SOUTH_EASTERN_ASIA = "SEAS", _("South-eastern Asia")
+        EASTERN_ASIA = "E_AS", _("Eastern Asia")
+        MELANESIA = "MELA", _("Melanesia")
+        MICRONESIA = "MICR", _("Micronesia")
+        CENTRAL_ASIA = "C_AS", _("Central Asia")
 
     numeric_code = models.PositiveSmallIntegerField(validators=[MaxValueValidator(1000)], null=False, unique=True)
     alpha2_code = models.CharField(max_length=2, null=False, unique=True)
@@ -24,10 +46,15 @@ class Country(models.Model):
     official_name = models.CharField(max_length=50, null=False, unique=True)
     common_name = models.CharField(max_length=50, null=False, unique=True)
     continent = models.CharField(max_length=2, choices=Continent.choices, default=Continent.ANTARCTICA, null=False)
+    region = models.CharField(max_length=4, choices=Region.choices, default=Region.ANTARCTICA, null=False)
 
     @property
     def continent_name(self):
         return self.Continent(self.continent).label
+
+    @property
+    def region_name(self):
+        return self.Region(self.region).label
 
     def __str__(self):
         return f"{self.common_name} ({self.continent_name})"
