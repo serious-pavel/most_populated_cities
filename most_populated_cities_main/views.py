a271c7428c8b82afb37f5e26e264a83e9a1210ac
pavel.makhnev <p_makhnev@ural-press.ru>
2023-11-01 13:47:30 +0100

    add views for country and city pages

--- most_populated_cities_main/views.py
+++ most_populated_cities_main/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Country, City
 
 # Create your views here.
 
 
-def main(request):
-    return render(request, 'most_populated_cities_main/table_cities.html')
+def city_page(request):
+    cities = City.objects.all()
+    return render(request, 'most_populated_cities_main/table_cities.html', context={'cities': cities})
 
+
+def country_page(request):
+    countries = Country.objects.all()
+    return render(request, 'most_populated_cities_main/table_countries.html', context={'countries': countries})
