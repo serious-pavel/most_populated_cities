c14bb4fedd71ec2a20618e4f91d550d105f07a2f
pavel.makhnev <p_makhnev@ural-press.ru>
2023-11-01 13:47:42 +0100

    add urls for country and city pages

--- most_populated_cities_main/urls.py
+++ most_populated_cities_main/urls.py
@@ -6,5 +6,7 @@ from . import views
 app_name = 'most_populated_cities_main'
 
 urlpatterns = [
-    path('', views.main, name='main')
+    path('', views.city_page, name='cities'),
+    path('countries', views.country_page, name='countries'),
+
 ]
