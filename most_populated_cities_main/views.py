5d9e7ac78dbd192a2c89ca2911846663d010894e
pavel.makhnev <p_makhnev@ural-press.ru>
2023-11-11 14:31:48 +0100

    implement pagination for countries, basics from documentation

--- most_populated_cities_main/views.py
+++ most_populated_cities_main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Country, City
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -11,4 +12,11 @@ def city_page(request):
 
 def country_page(request):
     countries = Country.objects.all().order_by('common_name')
-    return render(request, 'most_populated_cities_main/table_countries.html', context={'countries': countries})
+    paginator = Paginator(countries, 20)  # Show 25 contacts per page.
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    item_counter = (page_obj.number - 1) * paginator.per_page
+
+    return render(request, 'most_populated_cities_main/table_countries.html',
+                  context={'page_obj': page_obj, 'item_counter': item_counter})
