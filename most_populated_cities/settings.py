98e2fe24c226e6fc1f9d14896cd8149eae0c54a6
serious-pavel <pvmakhnev@gmail.com>
2024-09-01 18:17:48 +0200

    get right credentials in dependence on environment

--- most_populated_cities/settings.py
+++ most_populated_cities/settings.py
@@ -121,13 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-if env.str('DB_HOST') == '127.0.0.1':
+if os.path.exists('dev/CREDENTIALS_FILE'):
     from google.oauth2 import service_account
     credentials = service_account.Credentials.from_service_account_file('dev/CREDENTIALS_FILE')
 else:
     from google.auth import compute_engine
     credentials = compute_engine.Credentials()
-
 
 STORAGES = {
     "staticfiles": {
