ef162390939afc37d170e9c53d6ced36d87fc633
serious-pavel <pvmakhnev@gmail.com>
2024-09-01 19:44:06 +0200

    migrate to gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 ./manage.py update_or_create_countries
 ./manage.py update_or_create_cities
 ./manage.py collectstatic --noinput
-gunicorn.exe most_populated_cities.wsgi:application --bind 0.0.0.0:8080
+gunicorn most_populated_cities.wsgi:application --bind 0.0.0.0:8080
 #./manage.py runserver 0.0.0.0:8080 --nostatic
