c38e2e2fa2b58c0d21a3c46140e29ffcd5636413
serious-pavel <pvmakhnev@gmail.com>
2024-09-01 14:32:22 +0200

    disable staticfiles serving by django developing server

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 ./manage.py update_or_create_cities
 ./manage.py collectstatic --noinput
 #gunicorn myproject.wsgi:application --bind 0.0.0.0:8000
-./manage.py runserver 0.0.0.0:8080
+./manage.py runserver 0.0.0.0:8080 --nostatic
